050161cbcdb234d86390bab84e54c7e80ead093a
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-09-12 15:20:07 +0700

    element

--- lession1/main.js
+++ lession1/main.js
@@ -1,110 +1,89 @@
-// biến: var let const 
+//Biến: var let const
 
 // camel case
-var myName;
+var myName="chinhpd5";
 
-// pascal case
-var MyName;
+// Pascal case
+var MyName ="chinhpd5";
 
-// khai báo;
-var myName = "chinhpd5";
-var myName = "chinhpd6";
+// Khai báo
+var myName = "xin chào"
 
-let myAge = 30;
-myAge = 40;
+let hello ='Hello';
+hello = "xin chào";
 
 // scope
-
-// global scope (var let const)
-
-let myHome ="Hà Nội";
+// global scope
+let myName1 = "chinhpd5"; // var const
 
 {
-    // console.log(myHome);
+    // console.log(myName1);
 }
 
-// block scope (if else, switch case, for)
-
+// block scope (if else switch for)
 {
     var a = 10;
 }
-
 // console.log(a); // có thể
 
 {
-    let b = 20; // const
-    // console.log(b);
+    let b = 20;
 }
-
-// console.log(b);  // ko thể
+// console.log(b); // không thể
 
 // local scope (function)
 function sayHello(){
-    var c = 30; // let const
+    var c =30; // let const
 }
 
 // console.log(c);
 
-// Hoisting
+// hoisting
 
 d = 50;
 // console.log(d);
 var d;
 
-
 // console.log(e);
-let e;
+let e =20;
 
-const PI = 3.14;
-// PI =4;
-const OBJ = {name: "chinhpd5"}
+// const
+const PI = '3.14';
+// PI = 4;
 
-OBJ.name ="chinhpd6"
-
-// console.log(OBJ);
-
-// Kiểu dữ liệu
+// Kiểu dữ liệu trong JS
 /**
- * Kiểu dữ liệu nguyên thủy:
- * - number: 1, -2, 1.5
- * - string: '', "", ``
+ * Kiểu dữ liệu nguyên thủy
+ * - string: '' "" ``
+ * - number: 1 -1 1.5
  * - boolean: true false
  * - null
  * - undefinded
  * - symbol
  * - bigInt
+ 
  * Kiểu dữ liệu phức tạp
  * - object
  * - array
  */
 
-// Toán tử:
+
+// Toán tử
 /**
- * Toán tử số học: + - * / ** % ++ -- ...
+ * Toán tử số học: + - * / ** % ++ --
  * Toán tử so sánh: > < >= <= == != === !==
- * Toán tử logic: && || !
  * Toán tử gán: = += (i+=1 <=> i = i + 1) -= *= ...
+ * Toán tử logic: && || !
  */
 
-// Biểu thức Logic
-// Biểu thức điều kiện
-/**
- * if else
- * switch case
- * Toán tử 3 ngôi
- */
 
-var result = (1 == true) ? 'Đúng':'Sai'
+//Cú pháp
+// Cú pháp điều kiện : if else, switch case, toán tử 3 ngôi
+var result = (1 == true) ? "Đúng" : "Sai"
 
 // Vòng lặp
-/**
- * while
- * do while
- * forEach
- * for
- * for of
- * for in
- */
+// for
+// while, do while, forEach, for of, for in
 
 // DOM (Document Object Model)
 
@@ -115,71 +94,30 @@ var result = (1 == true) ? 'Đúng':'Sai'
  * css selectors
  */
 
-var h1Element = document.getElementById('heading')
-// console.log({value:h1Element});
+var h1Element = document.getElementById("heading")
+// console.log({h1Element});
 
-var pElements = document.getElementsByClassName('paragragh')
+var h1Elements = document.getElementsByClassName('title')
+// console.log(h1Elements);
+var pElements = document.getElementsByClassName('paragragh');
 // console.log(pElements);
+
+// for(var i =0;i <pElements.length; i++){
+//     console.log(pElements[i]);
+// }
+
 // pElements.forEach(function(item){
 //     console.log(item);
 // })
+var pElements = document.getElementsByTagName('p');
+console.log(pElements);
 
-for(let i =0; i < pElements.length; i++){
-    // console.log(pElements[i]);
-}
-
-var h1Elements = document.getElementsByClassName('title');
-// console.log(h1Elements);
-
-var pElements = document.getElementsByTagName('p')
-// console.log(pElements);
-//
-var h1Element = document.querySelector('#heading');// id
-var h1Element = document.querySelector('.title');// class
-var h1Element = document.querySelector('h1');// tag
-
-// console.log(h1Element);
-
-var pElements = document.querySelectorAll('.paragragh')
-// console.log(pElements);
-pElements.forEach(function(item){
-    // console.log(item);
-})
 
 
 
 // 2. Attribute
-var h1Element = document.querySelector('#heading');
-
-h1Element.id = "heading2"
-// console.log(h1Element.id);
-
-h1Element.style.color = 'red';
-
-h1Element.setAttribute("data-id","chinhpd5")
-
-// console.log(h1Element.getAttribute("data-id"));
-
 
 // 3. Text
-var h1Element = document.querySelector('#heading');
-
-// console.log(h1Element.innerText);
-// console.log(h1Element.textContent);
-
-// h1Element.innerText = `
-// Xin chào
-// `
-
-// h1Element.textContent = `
-// <i>Xin chào</i>
-// `
-
-h1Element.innerHTML = `
-<i>Xin chào</i>
-`
-
-
 
 
 
