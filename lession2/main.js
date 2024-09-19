fd3168e27ba6b489537ff1a6302cb82daf2d709a
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-09-19 14:48:54 +0700

    destructuring

--- lession2/main.js
+++ lession2/main.js
@@ -26,4 +26,49 @@ var sayHello4 = name => `Hi ${name}`
 // console.log(sayHello3("chinhpd5",20));
 // console.log(sayHello4("Bắc"));
 
+// Default Parameter
+var total = (a=0,b=0) => a + b;
+
+// console.log(total(10,15));
+
+//  Destructuring
+//aray
+var arr = [1,2,3]
+
+var [a,,c] = arr;
+// console.log(a); //1
+// // console.log(b); //2
+// console.log(c); //3
+
+//object
+var info  ={
+    name:"chinhpd5",
+    age:30,
+    child: {
+        name: "chinhp6"
+    }
+}
+
+// var {name,age, child:{name: childName}} = info;
+// console.log(name);
+// console.log(age);
+// console.log(childName);
+
+function showInfo({name,age, child:{name: childName}}){
+    console.log(name);
+    console.log(age);
+    console.log(childName);
+}
+
+showInfo(info)
+
+
+
+
+
+
+
+
+
+
 
