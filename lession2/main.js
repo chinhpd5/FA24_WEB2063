c6f626b43b3886b6104f97eaa22db14398c75583
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-09-21 08:06:23 +0700

    default parameter, destructuring

--- lession2/main.js
+++ lession2/main.js
@@ -25,6 +25,52 @@ var sayHello3 = (name,age) => {
 // console.log(sayHello3("chinhpd5",20));
 var sayHello4 = name => `Xin chào ${name}`
 
-console.log(sayHello4("chinpd5"));
+// console.log(sayHello4("chinpd5"));
+
+// Default parameter
+
+var total = (a=0,b=0) => a +b;
+
+// console.log(total(10,20));
+
+// Destructuring
+var arr =[1,2,3,4];
+
+var [a,,c] = arr;
+
+// console.log(a);//1
+// // console.log(b);// 2
+// console.log(c);//3
+// console.log(d);
+
+var info ={
+    name: "chinhpd5",
+    age: 20,
+    child:{
+        name: "chinhpd6"
+    }
+}
+
+var {name,age,child:{name: childName}} = info;
+
+// console.log(name);
+// console.log(age);
+// console.log(childName);
+
+const showInfo =({name,age,child:{name: childName}})=>{
+    console.log(name);
+    console.log(age);
+    console.log(childName);
+}
+
+showInfo(info)
+
+
+
+
+
+
+
+
 
 
