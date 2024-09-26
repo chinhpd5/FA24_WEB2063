ff92f9f52666f96c0ef7ac85634b85a3d39d515c
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-09-26 15:18:58 +0700

    map

--- lession3/main.js
+++ lession3/main.js
@@ -60,7 +60,7 @@ list.forEach((item,index)=>{
 
 const tbodyElement = document.querySelector('tbody');
 
-tbodyElement.innerHTML = trElements;
+// tbodyElement.innerHTML = trElements;
 
 // find: duyệt qua mảng và tìm kiếm 1 phần tử gần nhất trong mảng thỏa mãn điều kiện *return
 // nếu có ít nhất 1 phần tử -> kết thúc vòng lặp
@@ -92,11 +92,31 @@ const checkEvery = list.every((item,index)=>{
 // nếu tất cả các phần tử KHÔNG thỏa mãn ĐK (return)-> false
 
 const checkSome = list.some((item,index)=>{
-    console.log(index);
+    // console.log(index);
     return item.mark < 5; // item.gender == false
 })
 
-console.log(checkSome);
+// console.log(checkSome);
+
+
+// map: Duyệt qua toàn bộ các phần tử trong mảng
+// có thể trả về 1 mảng mới thông qua return;
+
+trElements = list.map((item,index)=>{
+    return `
+        <tr>
+            <th scope="row">${index+1}</th>
+            <td>${item.name}</td>
+            <td>${item.age}</td>
+            <td>${ item.gender ? "Nam": "Nữ" }</td>
+            <td>${item.mark}</td>
+        </tr>
+    `
+}).join("");//join: chuyển 1 mảng về 1 chuỗi
+
+console.log(trElements);
+tbodyElement.innerHTML = trElements;
+
 
 
 
