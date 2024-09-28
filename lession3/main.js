ac6b6c89ff911cc1f3f035f6a21f4f922c10f8ae
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-09-28 09:40:34 +0700

    start

---
+++ lession3/main.js
@@ -0,0 +1,42 @@
+// Các phương thức làm việc với mảng
+
+/**
+ * forEach
+ * find
+ * every
+ * some
+ * map
+ * filter
+ * reduce
+ */
+
+const list = [
+    {
+        name: "chinhpd5",
+        age:20,
+        gender: true,
+        mark: 10
+    },
+    {
+        name: "chinhpd6",
+        age:21,
+        gender: false,
+        mark:7
+    },
+    {
+        name: "chinhpd8",
+        age:19,
+        gender: true,
+        mark: 9
+    },
+    {
+        name: "chinhpd9",
+        age:20,
+        gender: false,
+        mark: 9
+    }
+]
+
+// forEach: duyệt qua toàn bộ các phần tử trong mảng
+
+
