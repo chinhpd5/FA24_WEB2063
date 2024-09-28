b4965b1a252a562b9a6fe65f94a0f257a1d3a4b8
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-09-28 07:47:33 +0700

    forach

---
+++ lession3/main.js
@@ -0,0 +1,60 @@
+// Các phương thức làm việc với mảng
+
+/**
+ * forEach
+ * find
+ * some
+ * every
+ * map
+ * filter
+ * reduce
+ */
+
+const list = [
+    {
+        name: "chinhpd5",
+        age:20,
+        gender: true,
+        mark: 10
+    },
+    {
+        name: "chinhpd6",
+        age:21,
+        gender: false,
+        mark:7
+    },
+    {
+        name: "chinhpd8",
+        age:19,
+        gender: true,
+        mark: 9
+    },
+    {
+        name: "chinhpd9",
+        age:20,
+        gender: false,
+        mark: 9
+    }
+]
+
+// forEach: duyệt qua toàn bộ phần tử trong mảng
+
+let trElements = '';
+list.forEach((item,index)=>{
+    // console.log(item);// giá trị của các phần tử trong mảng
+    // console.log(index); // vị trí của các phần tử trong mảng
+    trElements += `
+        <tr>
+            <th scope="row">${index+1}</th>
+            <td>${item.name}</td>
+            <td>${item.age}</td>
+            <td>${item.gender ? "Nam": "Nữ"}</td>
+            <td>${item.mark}</td>
+        </tr>
+    `
+})
+
+console.log(trElements);
+const tbobyElement= document.querySelector('tbody');
+
+tbobyElement.innerHTML = trElements;
