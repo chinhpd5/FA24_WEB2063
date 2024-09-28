908c8130875bdd4ffccb90cf631491e5ed95dea3
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-09-28 09:55:37 +0700

    foreach

--- lession3/main.js
+++ lession3/main.js
@@ -39,4 +39,22 @@ const list = [
 
 // forEach: duyệt qua toàn bộ các phần tử trong mảng
 
+let trElements =''
+list.forEach((item,index)=>{
+    // console.log(item); // giá trị của các phần tử
+    // console.log(index);// vị trí của các phần tử
+    trElements += `
+        <tr>
+            <th scope="row">${index+1}</th>
+            <td>${item.name}</td>
+            <td>${item.age}</td>
+            <td>${item.gender ? "Nam":"Nữ"}</td>
+            <td>${item.mark}</td>
+        </tr>
+    `
+})
+console.log(trElements);
+const tbodyElement = document.querySelector('tbody')
+tbodyElement.innerHTML = trElements
+
 
